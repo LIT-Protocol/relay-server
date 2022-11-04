6ee6e14b1f00bdbcd4bae0ae81bc1889852ea3d5
Howard <hwrdtm@users.noreply.github.com>
2022-11-03 18:37:42 -0700

    implement IP-based rate limiting with express (#2)
    * implement IP-based rate limiting with express
    
    * remove comment

---
+++ config/index.ts
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+import { Config } from "../models";
+
+const { REDIS_URL, PORT } = process.env;
+
+const config: Config = {
+    redisUrl: REDIS_URL || "dummy-url",
+    port: parseInt(PORT === "" ? PORT : "3000"),
+};
+
+export default config;
