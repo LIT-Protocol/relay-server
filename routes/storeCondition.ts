b1e0822b68c895b95996c7607a62eb94e11669d3
Howard <hwrdtm@users.noreply.github.com>
2022-11-25 17:32:35 -0800

    Implement session sig verification (#3)
    * Implement INCOMPLETE session sig verification
    
    * fix ed25519 verification
    
    * enable and fix SIWE Resources verification
    
    * take in string req body params, add cors, fixes
    
    * fix missing import
    
    * auto linting adn formatting
    
    Co-authored-by: Chris Cassano <1285652+glitch003@users.noreply.github.com>

--- routes/storeCondition.ts
+++ routes/storeCondition.ts
@@ -1,91 +1,126 @@
-import { utils } from 'ethers';
-import { Request } from 'express';
-import { Response } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
-import { storeConditionWithSigner } from '../lit';
-import { StoreConditionRequest, StoreConditionResponse } from '../models';
+import { Request } from "express";
+import { Response } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import { storeConditionWithSigner } from "../lit";
+import {
+  CapabilityProtocolPrefix,
+  StoreConditionRequest,
+  StoreConditionResponse,
+} from "../models";
+import { getFullResourceUri, validateSessionSignature } from "../utils/auth";
 
-const BYTE_ARRAY_LENGTH = 32;
+const BYTE_STRING_LENGTH = 64;
 
 // TODO: Change into async post (with getter API) to handle more concurrent requests.
 export async function storeConditionHandler(
-    req: Request<{}, StoreConditionResponse, StoreConditionRequest, ParsedQs, Record<string, any>>,
-    res: Response<StoreConditionResponse, Record<string, any>, number>,
+  req: Request<
+    {},
+    StoreConditionResponse,
+    StoreConditionRequest,
+    ParsedQs,
+    Record<string, any>
+  >,
+  res: Response<StoreConditionResponse, Record<string, any>, number>
 ) {
-    // Verify auth sig
-    const { signedMessage, sig, address } = req.body.authSig;
-    const creatorAddress = utils.verifyMessage(signedMessage, sig);
-    if (creatorAddress != address) {
-        return res.status(401).json({
-            error: "Invalid authSig"
-        });
-    }
-    console.info("Verified creator", { creatorAddress });
+  // Validate capability protocol prefix.
+  if (
+    req.body.capabilityProtocolPrefix !==
+      CapabilityProtocolPrefix.LitEncryptionCondition.toString() &&
+    req.body.capabilityProtocolPrefix !==
+      CapabilityProtocolPrefix.LitSigningCondition.toString()
+  ) {
+    return res.status(400).json({
+      error: `Only the following capability protocol prefixes are supported: ${[
+        CapabilityProtocolPrefix.LitEncryptionCondition,
+        CapabilityProtocolPrefix.LitSigningCondition,
+      ]}`,
+    });
+  }
 
-    // Validate request body
-    let validationError = validateRequest(req.body);
-    if (!!validationError) {
-        return res.status(400).json({
-            error: validationError.toString()
-        });
-    }
+  // Validate session signature.
+  const fullResourceUri = getFullResourceUri(
+    req.body.capabilityProtocolPrefix,
+    req.body.key.replace("0x", "")
+  );
+  const [creatorAddress, validationErr] = await validateSessionSignature(
+    req.body.sessionSig,
+    fullResourceUri,
+    req.body.capabilityProtocolPrefix
+  );
+  if (!!validationErr) {
+    console.error("Invalid sessionSig", { error: validationErr });
+    return res.status(401).json({
+      error: "Invalid sessionSig",
+    });
+  }
+  console.info("Verified creator", { creatorAddress });
 
-    // Call into AccessControlConditions.storeConditionWithSigner()
-    try {
-        const {
-            key,
-            value,
-            securityHash,
-            chainId,
-            permanent,
-        } = req.body;
+  // Validate request body
+  let validationError = validateRequest(req.body);
+  if (!!validationError) {
+    return res.status(400).json({
+      error: validationError.toString(),
+    });
+  }
 
-        const storeTx = await storeConditionWithSigner({
-            key,
-            value,
-            securityHash,
-            chainId,
-            permanent,
-            creatorAddress,
-        });
+  // Call into AccessControlConditions.storeConditionWithSigner()
+  try {
+    const { key, value, securityHash, chainId, permanent } = req.body;
 
-        return res.status(201).json({
-            txHash: storeTx.hash
-        });
-    } catch (err) {
-        console.error("Unable to store condition with signer", { err });
-        return res.status(500).end();
-    }
+    const storeTx = await storeConditionWithSigner({
+      key,
+      value,
+      securityHash,
+      chainId,
+      permanent,
+      creatorAddress,
+    });
+
+    return res.status(201).json({
+      txHash: storeTx.hash,
+    });
+  } catch (err) {
+    console.error("Unable to store condition with signer", { err });
+    return res.status(500).end();
+  }
 }
 
 function validateRequest(requestBody: StoreConditionRequest): Error | null {
-    const keysToCheckExist: Array<keyof StoreConditionRequest> = ["key", "value", "securityHash", "chainId", "permanent"];
-    const keysToCheckByteArrayLength: Array<keyof Pick<StoreConditionRequest, "key" | "value" | "securityHash">> = ["key", "value", "securityHash"];
+  const keysToCheckExist: Array<keyof StoreConditionRequest> = [
+    "key",
+    "value",
+    "securityHash",
+    "chainId",
+    "permanent",
+  ];
+  const keysToCheckByteStringLength: Array<
+    keyof Pick<StoreConditionRequest, "key" | "value" | "securityHash">
+  > = ["key", "value", "securityHash"];
 
-    // Check values exist
-    const requestBodyKeys = Object.keys(requestBody);
-    for (const k of keysToCheckExist) {
-        if (requestBodyKeys.indexOf(k) === -1) {
-            return new Error(`${k} not provided`);
-        }
+  // Check values exist
+  const requestBodyKeys = Object.keys(requestBody);
+  for (const k of keysToCheckExist) {
+    if (requestBodyKeys.indexOf(k) === -1) {
+      return new Error(`${k} not provided`);
     }
+  }
 
-    // Check 32 bytes long
-    for (const k of keysToCheckByteArrayLength) {
-        if (requestBody[k].length != BYTE_ARRAY_LENGTH) {
-            return new Error(`${k} is not 32 long`);
-        }
+  // Check 32 bytes long
+  for (const k of keysToCheckByteStringLength) {
+    if (requestBody[k].length != BYTE_STRING_LENGTH) {
+      return new Error(`${k} is not ${BYTE_STRING_LENGTH} long`);
     }
+  }
 
-    // Check chainId is valid.
-    if (!validateChainId(requestBody.chainId)) {
-        return new Error("chainId invalid");
-    }
+  // Check chainId is valid.
+  if (!validateChainId(requestBody.chainId)) {
+    return new Error("chainId invalid");
+  }
 
-    return null;
+  return null;
 }
 
 function validateChainId(chainId: number): boolean {
-    // TODO: something more sophisticated?
-    return true;
+  // TODO: something more sophisticated?
+  return true;
 }
