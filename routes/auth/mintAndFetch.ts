b44e14d91611a252c34ed3c24edc8f5c5a4e6955
Howard Tam <howard.tam.95@gmail.com>
2023-07-14 16:49:51 -0700

    Add unified endpoint for mintNextAndAddAuthMethod

---
+++ routes/auth/mintAndFetch.ts
@@ -0,0 +1,81 @@
+import { Request } from "express";
+import { Response } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import { getPKPsForAuthMethod, mintPKPV2 } from "../../lit";
+import {
+	AuthMethodVerifyToFetchResponse,
+	FetchRequest,
+	MintNextAndAddAuthMethodsRequest,
+	MintNextAndAddAuthMethodsResponse,
+} from "../../models";
+
+export async function mintNextAndAddAuthMethodsHandler(
+	req: Request<
+		{},
+		MintNextAndAddAuthMethodsResponse,
+		MintNextAndAddAuthMethodsRequest,
+		ParsedQs,
+		Record<string, any>
+	>,
+	res: Response<
+		MintNextAndAddAuthMethodsResponse,
+		Record<string, any>,
+		number
+	>,
+) {
+	// mint PKP for user
+	try {
+		const mintTx = await mintPKPV2(req.body);
+		console.info("Minted PKP", {
+			requestId: mintTx.hash,
+		});
+		return res.status(200).json({
+			requestId: mintTx.hash,
+		});
+	} catch (err) {
+		console.error("Unable to mint PKP", {
+			err,
+		});
+		return res.status(500).json({
+			error: `Unable to mint PKP`,
+		});
+	}
+}
+
+// Fetch PKPs for verified Discord account
+export async function fetchPKPsHandler(
+	req: Request<
+		{},
+		AuthMethodVerifyToFetchResponse,
+		FetchRequest,
+		ParsedQs,
+		Record<string, any>
+	>,
+	res: Response<AuthMethodVerifyToFetchResponse, Record<string, any>, number>,
+) {
+	// get Discord access token from body
+	const { authMethodType, authMethodId } = req.body;
+
+	try {
+		const pkps = await getPKPsForAuthMethod({
+			authMethodType: authMethodType,
+			idForAuthMethod: authMethodId,
+		});
+		console.info("Fetched PKPs with auth type", authMethodType, {
+			pkps: pkps,
+		});
+		return res.status(200).json({
+			pkps: pkps,
+		});
+	} catch (err) {
+		console.error(
+			`Unable to fetch PKPs for given auth type ${authMethodType}`,
+			{
+				err,
+			},
+		);
+		return res.status(500).json({
+			error: `Unable to fetch PKPs for given auth method type: ${authMethodType}`,
+		});
+	}
+}
