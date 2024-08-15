d74e5f9db74ec49225aed5cbb8d86ce232fe3f86
Jatin Gupta <jatin@threely.io>
2024-08-15 15:59:48 +0530

    env added in sentry

--- instrument.ts
+++ instrument.ts
@@ -3,4 +3,5 @@ const Sentry = require("@sentry/node");
 
 Sentry.init({
   dsn: "https://ccf59999fa8b1604370e21a60e6b0565@o4505837187366912.ingest.us.sentry.io/4507728950722560",
+  environment: process.env.NODE_ENV || 'production'
 });
