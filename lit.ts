d176afe5239d245b850f3189a23f52f36b43619e
awisniew207 <awisniew@uoregon.edu>
2025-02-19 11:50:34 -0800

    lit to lit.ts

--- lit.ts
+++ lit.ts
@@ -10,15 +10,22 @@ import {
 import { Sequencer } from "./lib/sequencer";
 import { parseEther } from "ethers/lib/utils";
 import { CapacityToken } from "lit";
-import { LIT_NETWORK_VALUES } from "@lit-protocol/constants";
+import { LIT_NETWORK_VALUES, LIT_NETWORKS } from "@lit-protocol/constants";
+import { LitNodeClientNodeJs } from "@lit-protocol/lit-node-client-nodejs";
+import { PKPEthersWallet } from "@lit-protocol/pkp-ethers";
+import { LitActionResource, LitPKPResource } from "@lit-protocol/auth-helpers";
+import { LIT_ABILITY } from "@lit-protocol/constants";
+import { LIT_NETWORKS_KEYS } from "@lit-protocol/types";
+import { estimateGasWithBalanceOverride, removeTxnSignature, txnToBytesToSign } from "./utils/eth";
 
 import {
 	datil,
 	datilDev,
-	datilTest,
-	habanero,
-	manzano,
+	datilTest
 } from "@lit-protocol/contracts";
+
+// Cast the network to LIT_NETWORK_VALUES type at the config level
+config.network = config.network as unknown as LIT_NETWORK_VALUES;
 
 function getContractFromJsSdk(
 	network: LIT_NETWORK_VALUES,
@@ -29,18 +36,11 @@ function getContractFromJsSdk(
 
 	let contractsDataRes;
 	switch (network) {
-		case "manzano":
-			contractsDataRes = manzano;
-			break;
-		case "habanero":
-			contractsDataRes = habanero;
-			break;
 		case "datil-dev":
 			contractsDataRes = datilDev;
 			break;
 		case "datil-test":
 			contractsDataRes = datilTest;
-
 			break;
 		case "datil":
 			contractsDataRes = datil;
@@ -118,33 +118,20 @@ function getContract(abiPath: string, deployedContractAddress: string) {
 	return ethersContract;
 }
 
+
+// No datildev support
 function getAccessControlConditionsContract() {
 	switch (config.network) {
-		case "serrano":
+		case "serrano" as LIT_NETWORK_VALUES:
 			return getContract(
 				"./contracts/serrano/AccessControlConditions.json",
-				config?.serranoContract
-					?.accessControlConditionsAddress as string,
+				config?.serranoContract?.accessControlConditionsAddress as string,
 			);
-		case "cayenne":
+		case "cayenne" as LIT_NETWORK_VALUES:
 			return getContract(
 				"./contracts/cayenne/AccessControlConditions.json",
-				config?.cayenneContracts
-					?.accessControlConditionsAddress as string,
+				config?.cayenneContracts?.accessControlConditionsAddress as string,
 			);
-	}
-}
-
-function getPkpHelperContractAbiPath() {
-	if (config.useSoloNet) {
-		return "./contracts/serrano/SoloNetPKPHelper.json";
-	}
-
-	switch (config.network) {
-		case "serrano":
-			return "./contracts/serrano/PKPHelper.json";
-		case "cayenne":
-			return "./contracts/cayenne/PKPHelper.json";
 	}
 }
 
@@ -153,29 +140,13 @@ function getPkpNftContractAbiPath() {
 		return "./contracts/serrano/SoloNetPKP.json";
 	}
 	switch (config.network) {
-		case "serrano":
+		case "datil-dev":
 			return "./contracts/serrano/PKPNFT.json";
-		case "cayenne":
-			return "./contracts/cayenne/PKPNFT.json";
 	}
 }
 
-function getPkpHelperContract(network: string): ethers.Contract {
+function getPkpHelperContract(network: LIT_NETWORK_VALUES): ethers.Contract {
 	switch (network) {
-		case "serrano":
-			return getContract(
-				getPkpHelperContractAbiPath()!,
-				config?.serranoContract?.pkpHelperAddress as string,
-			);
-		case "cayenne":
-			return getContract(
-				getPkpHelperContractAbiPath()!,
-				config?.cayenneContracts?.pkpHelperAddress as string,
-			);
-		case "manzano":
-			return getContractFromJsSdk("manzano", "PKPHelper");
-		case "habanero":
-			return getContractFromJsSdk("habanero", "PKPHelper");
 		case "datil-dev":
 			return getContractFromJsSdk("datil-dev", "PKPHelper");
 		case "datil-test":
@@ -189,20 +160,6 @@ function getPkpHelperContract(network: string): ethers.Contract {
 
 function getPermissionsContract(): ethers.Contract {
 	switch (config.network) {
-		case "serrano":
-			return getContract(
-				"./contracts/serrano/PKPPermissions.json",
-				config?.serranoContract?.pkpPermissionsAddress as string,
-			);
-		case "cayenne":
-			return getContract(
-				"./contracts/cayenne/PKPPermissions.json",
-				config?.cayenneContracts?.pkpPermissionsAddress as string,
-			);
-		case "manzano":
-			return getContractFromJsSdk("manzano", "PKPPermissions");
-		case "habanero":
-			return getContractFromJsSdk("habanero", "PKPPermissions");
 		case "datil-dev":
 			return getContractFromJsSdk("datil-dev", "PKPPermissions");
 		case "datil-test":
@@ -216,10 +173,10 @@ function getPermissionsContract(): ethers.Contract {
 
 async function getPaymentDelegationContract() {
 	switch (config.network) {
-		case "manzano":
-			return getContractFromJsSdk("manzano", "PaymentDelegation");
-		case "habanero":
-			return getContractFromJsSdk("habanero", "PaymentDelegation");
+		case "datil-test":
+			return getContractFromJsSdk("datil-test", "PaymentDelegation");
+		case "datil":
+			return getContractFromJsSdk("datil", "PaymentDelegation");
 		default:
 			throw new Error(
 				"PaymentDelegation contract not available for this network",
@@ -227,22 +184,8 @@ async function getPaymentDelegationContract() {
 	}
 }
 
-export function getPkpNftContract(network: string): ethers.Contract {
+export function getPkpNftContract(network: LIT_NETWORK_VALUES): ethers.Contract {
 	switch (network) {
-		case "serrano":
-			return getContract(
-				getPkpNftContractAbiPath()!,
-				config?.serranoContract?.pkpNftAddress as string,
-			);
-		case "cayenne":
-			return getContract(
-				getPkpNftContractAbiPath()!,
-				config?.cayenneContracts?.pkpNftAddress as string,
-			);
-		case "manzano":
-			return getContractFromJsSdk("manzano", "PKPNFT");
-		case "habanero":
-			return getContractFromJsSdk("habanero", "PKPNFT");
 		case "datil-dev":
 			return getContractFromJsSdk("datil-dev", "PKPNFT");
 		case "datil-test":
@@ -626,7 +569,7 @@ export async function mintCapacityCredits({
 }: {
 	signer: ethers.Wallet;
 }) {
-	if (config.network === "serrano" || config.network === "cayenne") {
+	if (config.network === "datil-dev") {
 		throw new Error(
 			`Payment delegation is not available on ${config.network}`,
 		);
@@ -734,7 +677,7 @@ async function queryCapacityCredit(
 }
 
 export async function queryCapacityCredits(signer: ethers.Wallet) {
-	if (config.network === "serrano" || config.network === "cayenne") {
+	if (config.network === "datil-dev") {
 		throw new Error(
 			`Payment delegation is not available on ${config.network}`,
 		);
@@ -757,7 +700,7 @@ export async function addPaymentDelegationPayee({
 	wallet: ethers.Wallet;
 	payeeAddresses: string[];
 }) {
-	if (config.network === "serrano" || config.network === "cayenne") {
+	if (config.network === "datil-dev") {
 		throw new Error(
 			`Payment delegation is not available on ${config.network}`,
 		);
@@ -828,3 +771,66 @@ export async function addPaymentDelegationPayee({
 //   console.log("packed", packed);
 //   return packed;
 // }
+
+export async function initializeLitClient() {
+	const litNodeClient = new LitNodeClientNodeJs({
+		litNetwork: process.env.NETWORK as LIT_NETWORKS_KEYS,
+		debug: false
+	});
+	await litNodeClient.connect();
+	return litNodeClient;
+}
+
+export async function getPkpSessionSigs(litNodeClient: any, pkpPublicKey: string, authMethod: any) {
+	return litNodeClient.getPkpSessionSigs({
+		pkpPublicKey: pkpPublicKey,
+		chain: "ethereum",
+		authMethods: [authMethod],
+		expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
+		resourceAbilityRequests: [
+			{
+				resource: new LitActionResource("*"),
+				ability: LIT_ABILITY.LitActionExecution,
+			},
+			{
+				resource: new LitPKPResource("*"),
+				ability: LIT_ABILITY.PKPSigning,
+			},
+		],
+	});
+}
+
+export async function signWithPkp(litNodeClient: any, pkpPublicKey: string, sessionSigs: any, toSign: Uint8Array) {
+	const signingResult = await litNodeClient.pkpSign({
+		pubKey: pkpPublicKey,
+		sessionSigs,
+		toSign,
+	});
+
+	if (!signingResult || !signingResult.signature) {
+		throw new Error("Failed to get signature from PKP");
+	}
+
+	return signingResult;
+}
+
+export async function initializePkpWallet({
+	sessionSigs,
+	pkpPublicKey,
+	litNodeClient,
+	provider,
+}: {
+	sessionSigs: any;
+	pkpPublicKey: string;
+	litNodeClient: any;
+	provider: ethers.providers.Provider;
+}) {
+	const pkpWallet = new PKPEthersWallet({
+		controllerSessionSigs: sessionSigs,
+		pkpPubKey: pkpPublicKey,
+		litNodeClient,
+		provider
+	});
+	await pkpWallet.init();
+	return pkpWallet;
+}
--- routes/auth/pkpSign.ts
+++ routes/auth/pkpSign.ts
@@ -1,19 +1,20 @@
 import { Request } from "express";
 import { Response } from "express-serve-static-core";
 import { ParsedQs } from "qs";
-import { LitNodeClientNodeJs } from "@lit-protocol/lit-node-client-nodejs";
-import { LitActionResource, LitPKPResource } from "@lit-protocol/auth-helpers";
-import { getSigner } from "../../lit";
-import { LIT_NETWORKS_KEYS } from "@lit-protocol/types";
-import { LIT_ABILITY } from "@lit-protocol/constants";
 import * as ethers from "ethers";
-import { PKPEthersWallet } from "@lit-protocol/pkp-ethers";
 import { Sequencer } from "../../lib/sequencer";
 import {
 	estimateGasWithBalanceOverride,
 	removeTxnSignature,
 	txnToBytesToSign,
 } from "../../utils/eth";
+import {
+	getSigner,
+	initializeLitClient,
+	getPkpSessionSigs,
+	signWithPkp,
+	initializePkpWallet,
+} from "../../lit";
 
 interface PKPSignRequest {
 	toSign: string;
@@ -40,52 +41,27 @@ export async function pkpSignHandler(
 		if (!toSign || !pkpPublicKey || !authMethod) {
 			return res.status(400).json({
 				error: "Missing required parameters: toSign, pkpPublicKey, or authMethod",
-				signature: ""
+				signature: "",
 			});
 		}
 
 		// Initialize and connect to LitNodeClient
-		const litNodeClient = new LitNodeClientNodeJs({
-			litNetwork: process.env.NETWORK as LIT_NETWORKS_KEYS,
-			debug: false
-		});
-		await litNodeClient.connect();
+		const litNodeClient = await initializeLitClient();
 
 		// Get session signatures
-		const sessionSigs = await litNodeClient.getPkpSessionSigs({
-			pkpPublicKey: pkpPublicKey,
-			chain: "ethereum",
-			authMethods: [authMethod],
-			expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
-			resourceAbilityRequests: [
-			  {
-				resource: new LitActionResource("*"),
-				ability: LIT_ABILITY.LitActionExecution,
-			  },
-			  {
-				resource: new LitPKPResource("*"),
-				ability: LIT_ABILITY.PKPSigning,
-			  },
-			],
-		});
+		const sessionSigs = await getPkpSessionSigs(litNodeClient, pkpPublicKey, authMethod);
 
 		// Use litNodeClient to sign the message
-		const signingResult = await litNodeClient.pkpSign({
-			pubKey: pkpPublicKey,
+		const signingResult = await signWithPkp(
+			litNodeClient,
+			pkpPublicKey,
 			sessionSigs,
-			toSign: ethers.utils.arrayify(ethers.utils.hashMessage(toSign)),
-		});
-
-		if (!signingResult || !signingResult.signature) {
-			throw new Error("Failed to get signature from PKP");
-		}
+			ethers.utils.arrayify(ethers.utils.hashMessage(toSign))
+		);
 
 		// Verify the signature
 		const pkpEthAddress = ethers.utils.computeAddress(pkpPublicKey);
-		const recoveredAddress = ethers.utils.verifyMessage(
-			toSign,
-			signingResult.signature
-		);
+		const recoveredAddress = ethers.utils.verifyMessage(toSign, signingResult.signature);
 
 		if (recoveredAddress.toLowerCase() !== pkpEthAddress.toLowerCase()) {
 			throw new Error("Signature verification failed");
@@ -101,12 +77,13 @@ export async function pkpSignHandler(
 
 				// Parse and fix the transaction
 				const parsedTxn = JSON.parse(toSign);
-				
+
 				// Prevent direct ETH transfers - only allow contract interactions
-				if (parsedTxn.data === undefined || parsedTxn.data === '0x') {
+				if (parsedTxn.data === undefined || parsedTxn.data === "0x") {
 					return res.status(400).json({
-						error: "Direct ETH transfers are not allowed. This endpoint is only for contract interactions.",
-						signature: signingResult.signature
+						error:
+							"Direct ETH transfers are not allowed. This endpoint is only for contract interactions.",
+						signature: signingResult.signature,
 					});
 				}
 
@@ -122,50 +99,44 @@ export async function pkpSignHandler(
 				// First sign the transaction bytes
 				const txnWithoutSig = removeTxnSignature(fixedTxn);
 				const msgBytes = await txnToBytesToSign(txnWithoutSig);
-				
-				// Get a fresh signature for the transaction bytes
-				const txnSigningResult = await litNodeClient.pkpSign({
-					pubKey: pkpPublicKey,
-					sessionSigs,
-					toSign: msgBytes,
-				});
 
-				if (!txnSigningResult || !txnSigningResult.signature) {
-					throw new Error("Failed to get transaction signature from PKP");
-				}
+				// Get a fresh signature for the transaction bytes
+				const txnSigningResult = await signWithPkp(
+					litNodeClient,
+					pkpPublicKey,
+					sessionSigs,
+					msgBytes
+				);
 
 				// Split the signature into components
 				const sig = ethers.utils.splitSignature(txnSigningResult.signature);
-				
+
 				// Verify the signature matches the PKP address
 				const fromViaSignature = ethers.utils.recoverAddress(msgBytes, sig);
-				
+
 				if (fromViaSignature.toLowerCase() !== pkpEthAddress.toLowerCase()) {
 					return res.status(500).json({
-						error: "Invalid signature - the recovered signature does not match the PKP address",
-						signature: signingResult.signature
+						error:
+							"Invalid signature - the recovered signature does not match the PKP address",
+						signature: signingResult.signature,
 					});
 				}
 
 				// Check gas price sanity
 				const currentGasPrice = await provider.getGasPrice();
 				const ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE = 10;
-				const gasPriceFromClient = ethers.BigNumber.from(fixedTxn.gasPrice);
+				const gasPriceFromClient = fixedTxn.gasPrice;
 				if (
 					gasPriceFromClient.lt(
-						currentGasPrice
-							.mul(100 - ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE)
-							.div(100),
+						currentGasPrice.mul(100 - ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE).div(100)
 					) ||
 					gasPriceFromClient.gt(
-						currentGasPrice
-							.mul(100 + ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE)
-							.div(100),
+						currentGasPrice.mul(100 + ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE).div(100)
 					)
 				) {
 					return res.status(500).json({
 						error: `Invalid gas price - the gas price sent by the client of ${gasPriceFromClient.toString()} is not within ${ALLOWED_GAS_PRICE_DEVIATION_PERCENTAGE}% of the current gas price of ${currentGasPrice.toString()}`,
-						signature: signingResult.signature
+						signature: signingResult.signature,
 					});
 				}
 
@@ -175,26 +146,22 @@ export async function pkpSignHandler(
 						provider,
 						txn: fixedTxn,
 						walletAddress: pkpEthAddress,
-					}),
+					})
 				);
 
 				const ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE = 10;
-				const gasLimitFromClient = ethers.BigNumber.from(fixedTxn.gasLimit);
+				const gasLimitFromClient = fixedTxn.gasLimit;
 				if (
 					gasLimitFromClient.lt(
-						gasLimit
-							.mul(100 - ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE)
-							.div(100),
+						gasLimit.mul(100 - ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE).div(100)
 					) ||
 					gasLimitFromClient.gt(
-						gasLimit
-							.mul(100 + ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE)
-							.div(100),
+						gasLimit.mul(100 + ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE).div(100)
 					)
 				) {
 					return res.status(500).json({
 						error: `Invalid gas limit - the gas limit sent by the client of ${gasLimitFromClient.toString()} is not within ${ALLOWED_GAS_LIMIT_DEVIATION_PERCENTAGE}% of the gas limit we estimated of ${gasLimit.toString()}`,
-						signature: signingResult.signature
+						signature: signingResult.signature,
 					});
 				}
 
@@ -202,10 +169,7 @@ export async function pkpSignHandler(
 				const gasToFund = gasLimitFromClient.mul(gasPriceFromClient);
 				const gasFundingTxn = await sequencer.wait({
 					action: (...args) => {
-						const paramsToFn = Object.assign(
-							{},
-							...args,
-						) as ethers.providers.TransactionRequest;
+						const paramsToFn = Object.assign({}, ...args) as ethers.providers.TransactionRequest;
 						return signer.sendTransaction(paramsToFn);
 					},
 					params: [{ to: pkpEthAddress, value: gasToFund }],
@@ -214,13 +178,12 @@ export async function pkpSignHandler(
 				await gasFundingTxn.wait();
 
 				// Initialize PKP wallet for broadcasting
-				const pkpWallet = new PKPEthersWallet({
-					controllerSessionSigs: sessionSigs,
-					pkpPubKey: pkpPublicKey,
+				const pkpWallet = await initializePkpWallet({
+					sessionSigs,
+					pkpPublicKey,
 					litNodeClient,
-					provider
+					provider,
 				});
-				await pkpWallet.init();
 
 				// Broadcast the transaction using PKP wallet
 				const tx = await pkpWallet.sendTransaction({
@@ -230,7 +193,7 @@ export async function pkpSignHandler(
 					gasLimit: gasLimitFromClient,
 					gasPrice: gasPriceFromClient,
 					nonce: fixedTxn.nonce,
-					chainId: fixedTxn.chainId
+					chainId: fixedTxn.chainId,
 				});
 
 				console.info("Successfully signed and sent transaction", {
@@ -251,19 +214,17 @@ export async function pkpSignHandler(
 		console.info("Successfully signed and verified message with PKP", {
 			signature: signingResult.signature,
 			pkpEthAddress,
-			recoveredAddress
+			recoveredAddress,
 		});
 
 		return res.status(200).json({
 			signature: signingResult.signature,
 		});
 	} catch (err) {
-		console.error("[pkpSignHandler] Unable to sign message", {
-			err,
-		});
+		console.error("[pkpSignHandler] Unable to sign message", { err });
 		return res.status(500).json({
 			error: `[pkpSignHandler] Unable to sign message: ${JSON.stringify(err)}`,
-			signature: ""
+			signature: "",
 		});
 	}
 }
